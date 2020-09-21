c34261037a27336f7b22e64eed7dcc8e672edead
mcerrutti <mc.cerrutti@gmail.com>
2020-09-21 18:09:35 -0300

    La consigna

---
+++ Objetivo.py
@@ -0,0 +1,4 @@
+print("Se desea realizar para una entidad bancaria un sitema que permita"
+"calcular el monto final de un plazo fijo, donde el tiempo y el monto iicial"
+"se ingresa por teclado. Al finalizar se debe mostrar por pantalla el monto final la cantidad de meses por la que se calculo el plazo fijo y mostrar de cuanto es la tasa del inteneres anual")
+
